9e3e196fc0fc7a9929a374a764a8eafa7fdecfd8
chand-stack <chand.stack@gmail.com>
2023-09-01 03:37:32 +0600

    Chore: responsive card and time added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const allTabs = async () => {
     const tabContainer = document.getElementById('tab-container')
 
     tabName.forEach(element => {
-        // console.log(element)
+        
         const newTab = document.createElement('a')
         newTab.innerHTML = `
         <a onclick="cardList(${element.category_id})" class="tab bg-gray-300">${element.category}</a>
@@ -15,49 +15,93 @@ const allTabs = async () => {
 }
 
 const cardList = async (id) => {
-    // console.log(id)
+    
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id || '1000'}`)
     const data = await response.json();
     
     const cardDetails = data.data;
     // console.log(cardDetails)
+
+    const noContent = document.getElementById('no-content');
+    if (cardDetails.length === 0){
+        noContent.classList.remove('hidden');
+    }else{
+        noContent.classList.add('hidden')
+    }
+
     const cardContainer = document.getElementById('card-container')
     cardContainer.textContent='';
 
-    // if(element?.authors[0]?.verified == true){
-    //     document?.getElementById('badge')?.classList.remove('hidden');
-    // }else if(element?.authors[0]?.verified == false){
-    //     document?.getElementById('badge')?.classList.add('hidden')
-    // }
-    
 
     cardDetails.forEach(element=>{
-        console.log(element.authors[0])
+        // console.log(parseFloat(element.others.views))
+     
+
+        
+
+        
+
+
+        const secondToHour = (second) =>{
+            
+            const totalMinutes = Math. floor(second / 60)
+            const hours = Math. floor(totalMinutes / 60)
+            const minutes = totalMinutes % 60
+            // console.log('hours',hours, 'minute',minutes)
+            if(second === 0 || second === ''){
+                return ""
+            }else if(second !== 0 ){
+                return `${hours}hrs ${minutes} min ago`
+            }
+        }
+        // secondToHour(element.others.posted_date)
+        // console.log(secondToHour(element.others.posted_date))
         
         const newCard = document.createElement('div');
 
         
 
         newCard.innerHTML = `
-        <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img src="${element.thumbnail}" alt="PH Tube" /></figure>
+        <div class="card card-compact w-80 md:w-auto mx-auto md:mx-0 bg-base-100 shadow-xl">
+                    <figure class=""><img class=" h-52" src="${element.thumbnail}" alt="PH Tube" />
+                    <p class=" absolute left-36 top-44 md:left-40 lg:top-40 lg:left-44  top rounded-lg p-1 bg-black text-white ">${secondToHour(element.others.posted_date)}</p>
+                    </figure>
                     <div class="card-body flex flex-row">
-                        <div><img class="w-20 h-20 rounded-full" src="${element.authors[0].profile_picture}" alt=""></div>
+                        <div><img class="w-16 h-16 rounded-full" src="${element.authors[0].profile_picture}" alt="">
+                        </div>
+                        
                       <div>
+                      
                         <h2 class="card-title font-bold">${element.title}</h2>
-                      <p>${element.authors[0].profile_name} <span id="badge" class=""><i class="fa-solid fa-certificate text-blue-500"></i></span></p>
+                      <p class=" font-medium">${element.authors[0].profile_name} <span>${element.authors[0].verified ? `<i class="fa-solid fa-certificate text-blue-600"></i>` :''}</span></p>
                       <p>${element.others.views} views</p>
                       </div>
                       
                     </div>
                   </div>
         `;        
-       
         
-        cardContainer.appendChild(newCard)
-    })
+       
+        // console.log(newCard)
+                
+       
+     cardContainer.appendChild(newCard)
 
-}
+     
+        
+    
+        
+    
+    });
+
+// console.log(sortBtn(id))
+};
 cardList()
 
 allTabs()
+
+
+    
+const sortBtn =() =>{
+
+}
