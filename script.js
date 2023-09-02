6dafc3856b3c4e124696d15ac54fcf7ab3ee4579
chand-stack <chand.stack@gmail.com>
2023-09-02 12:41:09 +0600

    Fix: blog domain updated

--- script.js
+++ script.js
@@ -142,10 +142,15 @@ cardContainer.appendChild(newCard)
     
     }
 
-document.getElementById('blog-btn').addEventListener('click', function(){
-    document.location.href = 'http://127.0.0.1:5500/blog.html';
-})
+// document.getElementById('blog-btn').addEventListener('click', function(){
+//     document.location.href = 'http://127.0.0.1:5500/blog.html';
+// })
     
+const blogPage = "blog.html";
+
+document.getElementById("blog-btn").addEventListener("click", function () {
+  window.location.assign(blogPage);
+});
 
 
 const sortedTwo = (details) =>{
